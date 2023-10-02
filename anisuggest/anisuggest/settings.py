a48205bfd91c19e8f3095c76a9204c6b00005bc7
Marco Botelho <maob.botelho@gmail.com>
2023-10-02 19:47:21 -0300

    Pequenas correções no settings e forms

--- anisuggest/anisuggest/settings.py
+++ anisuggest/anisuggest/settings.py
@@ -118,17 +118,22 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
 USE_TZ = True
 
+USE_THOUSAND_SEPARATOR = True
+
+DECIMAL_SEPARATOR = ','
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
